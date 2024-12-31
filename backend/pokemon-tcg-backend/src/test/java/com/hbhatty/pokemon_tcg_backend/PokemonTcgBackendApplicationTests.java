7cd886920f15f1a83f94be885f59cbf5eee9952f
Harsh Bhatty <harshbhatty2@gmail.com>
2024-12-31 02:55:24 -0500

    Added template for backend - still need to fix gradle issue

---
+++ backend/pokemon-tcg-backend/src/test/java/com/hbhatty/pokemon_tcg_backend/PokemonTcgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbhatty.pokemon_tcg_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTcgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
