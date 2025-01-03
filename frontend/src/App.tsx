c5f77e796ddc3e2c48fcd877c41f830fadbb86cc
Harsh Bhatty <harshbhatty2@gmail.com>
2025-01-03 02:08:16 -0500

    Added frontend container to run in parallel and created a search bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
+import SearchBar from './components/Searchbar/SearchBar'
+const App = () => {
   const [count, setCount] = useState(0)
+
+  const handleSearch = async (query: string) => {
+    try {
+      // const results = await searchPokemonCards(query);
+      // setCards(results);
+    } catch (error) {
+      console.error("Error fetching cards:", error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="bg-red-500">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-           <span className="text-blue-500">count is {count}</span>
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <SearchBar onSearch={handleSearch} />
     </>
   )
 }
---
+++ frontend/src/components/Searchbar/SearchBar.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar = ({ onSearch }: SearchBarProps) => {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (query.trim() !== "") {
+      onSearch(query);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex items-center space-x-4">
+      <input
+        type="text"
+        placeholder="Search for a Pokémon card"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="p-2 border border-gray-300 rounded-md w-80"
+      />
+      <button
+        type="submit"
+        className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
+      >
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default SearchBar;
